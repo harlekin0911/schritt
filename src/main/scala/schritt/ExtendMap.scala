3f78df6c5753b730288f016745cb30781e6ff6b0
AD06076 <stefan.dickas@gmx.de>
2015-12-09 16:32:02 +0100

    Test extend Map immutable

--- src/main/scala/schritt/ExtendMap.scala
+++ src/main/scala/schritt/ExtendMap.scala
@@ -5,8 +5,18 @@ class ExtendMap[E,I>:Int]( m:Map[E,I])  extends Map[E,I] {
     def get( e:E) : Option[I] = m.get(e)
     def -(e:E) : ExtendMap[E,I] = new ExtendMap( m - e)
     def iterator : Iterator[(E,I)] = m.iterator
-    //def +[B1 >:I](kv:(E,B1)) : Map[E,B1] = m + kv
     def +[B1 >:I](kv:(E,B1)) : ExtendMap[E,B1] = new ExtendMap(m + kv)
+}
+
+class ExtendMap2[E]( m:Map[E,Int])  extends Map[E,Int] {
+    
+    def get( e:E) : Option[Int] = m.get(e)
+    def -(e:E) : ExtendMap2[E] = new ExtendMap2( m - e)
+    def iterator : Iterator[(E,Int)] = m.iterator
+    def +[B1 >:Int](kv:(E,B1)) : ExtendMap2[E] = kv._2 match {
+        case a:Int => new ExtendMap2(m + ((kv._1,a)))
+        case _     => throw new RuntimeException( "not allowed")
+    }
 }
 
 object ExtendMap {
@@ -17,5 +27,13 @@ object ExtendMap {
         
         val a = ExtendMap[String]
         val b = a + (( "hans", 3))
+        val c = b + (("peter", "bla")) 
+        val d = c + (("ursel", 2.0)) 
+        println( d)
+
+        val a2 = new ExtendMap2[String]( Map.empty)
+        val b2 = a2 + (( "hans", 3))
+        val c2 = b2 + (("peter", "bla")) 
+        println( c2)
     }
 }
